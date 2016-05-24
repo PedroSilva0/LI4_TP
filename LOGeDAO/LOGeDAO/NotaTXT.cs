c06c2e78a2b59d2de58588cf68ef82e4ddcaffbd
a72625 <rui.camposinhos@gmail.com>
2016-05-24 11:59:28 +0100

    classes de logica e dao (shared project)
    para usar como referencia nos projetos backoffice e mobile.
    ainda está incompleto

---
+++ LOGeDAO/LOGeDAO/NotaTXT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOGeDAO
+{
+    class NotaTXT
+    {
+        private int id { get;  }
+        private string descricao { get; set; }
+        private string texto { get; set; }
+
+        public NotaTXT(int mId, string mDesc, string mTXT)
+        {
+            id = mId;
+            descricao = mDesc;
+            texto = mTXT;
+        }
+    }
+}
---
+++ LOGeDAO/LOGeDAO/Resposta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOGeDAO
+{
+    class Resposta
+    {
+        private int idQuestao { get; }
+        private string resposta { get; set; }
+
+        public Resposta(int mQuestao, string mResp)
+        {
+            idQuestao = mQuestao;
+            resposta = mResp;
+        }
+    }
+}
