38598fa4671eb6a7e3977e7b90c80d8c54f4407f
Preg <pedrocas-silva@hotmail.com>
2016-06-17 00:22:32 +0100

    Mais duro!!!
    Agora já arrebenta menos vezes

--- BackOffice/BackOffice/ConsultarRelatorio.xaml.cs
+++ BackOffice/BackOffice/ConsultarRelatorio.xaml.cs
@@ -19,23 +19,25 @@ namespace BackOffice
     /// </summary>
     public partial class ConsultarRelatorio : Window
     {
-        
+        private LI4Entities data = new LI4Entities();
+
         public ConsultarRelatorio(int visita)
         {
             InitializeComponent();
             loadRelatorio(visita);
         }
 
-        private loadRelatorio(int visita)
+        private void loadRelatorio(int visita)
         {
-            private LI4Entities data = new LI4Entities();
+
             Visita aux = data.Visita.Find(visita);
             string relatorio = aux.html_file;
             wb2.NavigateToString(relatorio);
         }
-
-        
-            
-        
     }
+
+
+
+
 }
+
--- BackOffice/BackOffice/MainWindow.xaml.cs
+++ BackOffice/BackOffice/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Maps.MapControl.WPF;
+﻿
+using Microsoft.Maps.MapControl.WPF;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Media;
+using System.Net;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,16 +26,16 @@ namespace BackOffice
     /// </summary>
     public partial class MainWindow : Window
     {
-        //private LI4Entities data;
+        
         private facade fac;
-        private int id_rest;
-        private int id_planos;
-        private int id_fiscal = 1;
+        private int id_fiscal;
+        private bool inserir = true;
 
-        public MainWindow()
+        public MainWindow(int i)
         {
             InitializeComponent();
             fac = new facade();
+            id_fiscal = i;
             listarRestaurantes();
             listarTodosVoz();
             listaVisitas();
@@ -44,16 +47,22 @@ namespace BackOffice
             listView2.Items.Clear();
             fac = new facade();
             var lista_visita = fac.listarVisitas();
-
-            foreach (visitaDTO item in lista_visita)
+            if (lista_visita.Count() == 0)
             {
-                this.listView2.Items.Add(new visitaDTO
+                label8.Content = "Não existem inspeções concluidas";
+            }
+            else
+            {
+                foreach (visitaDTO item in lista_visita)
                 {
-                    id_vis = item.id_vis,
-                    desc = item.desc
-                    //descricao = item.id_est,
+                    this.listView2.Items.Add(new visitaDTO
+                    {
+                        id_vis = item.id_vis,
+                        desc = item.desc
 
-                });
+
+                    });
+                }
             }
         }
 
@@ -63,37 +72,43 @@ namespace BackOffice
             fac = new facade();
             var lista_relatorios = fac.listarRelatorios();
 
-            foreach (visitaDTO item in lista_relatorios)
+            if (lista_relatorios.Count() == 0)
             {
-                this.listView3.Items.Add(new visitaDTO
-                {
-                    id_vis = item.id_vis,
-                    desc = item.desc
-                });
+                label9.Content = "Não existem relatórios";
             }
+            else
+                foreach (visitaDTO item in lista_relatorios)
+                {
+                    this.listView3.Items.Add(new visitaDTO
+                    {
+                        id_vis = item.id_vis,
+                        desc = item.desc
+                    });
+                }
 
         }
 
-        
         private void listarRestaurantes()
         {
             listView.Items.Clear();
             fac = new facade();
             var lista_estabelecimento = fac.listarRestaurantes();
-            Estabelecimento e = lista_estabelecimento.ElementAt(0);
-            foreach (Estabelecimento item in lista_estabelecimento)
+            if (lista_estabelecimento.Count() == 0)
             {
-                
-                this.listView.Items.Add(new Estabelecimento
-                {
-                    nome = item.nome,
-                    id_est = item.id_est,
-                    morada = item.morada,
-                    latitude = item.latitude,
-                    longitude = item.longitude
-                });
+                label7.Content = "Não se encontram estabelecimentos registados";
             }
-            id_rest = lista_estabelecimento.Count+1;
+            else
+                foreach (Estabelecimento item in lista_estabelecimento)
+                {
+                    this.listView.Items.Add(new Estabelecimento
+                    {
+                        nome = item.nome,
+                        id_est = item.id_est,
+                        morada = item.morada,
+                        latitude = item.latitude,
+                        longitude = item.longitude
+                    });
+                }
         }
 
         private void listarTodosVoz()
@@ -101,54 +116,107 @@ namespace BackOffice
             listView1.Items.Clear();
             fac = new facade();
             var lista_voz = fac.listarTodosVoz();
-            foreach (Voz item in lista_voz)
+            if (lista_voz.Count() == 0)
             {
-                //Console.WriteLine("Ficheiro de voz");
-                this.listView1.Items.Add(new Voz
-                {
-                    descricao = item.descricao,
-                    id_voz = item.id_voz,
-                });
+                label6.Content = "Não existem ficheiros de voz";
             }
-            //id_rest = lista_estabelecimento.Count + 1;
+            else
+                foreach (Voz item in lista_voz)
+                {
+                    this.listView1.Items.Add(new Voz
+                    {
+                        descricao = item.descricao,
+                        id_voz = item.id_voz,
+                    });
+                }
         }
 
-
-
-
-
-        private void button6_Click(object sender, RoutedEventArgs e)
+        private void registar_estabelecimento_Click(object sender, RoutedEventArgs e)
         {
-            fac.registarRes(id_rest, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text);
-            listarRestaurantes();
-            label10.Content = "Restaurante adicionado com sucesso";
+            //bool inserir = true;
+            double latitude = 0;
+            double longitude = 0;
+            if(textBox3.Text.Equals("")|| textBox4.Text.Equals("") || textBox5.Text.Equals("") || textBox6.Text.Equals(""))
+            {
+                label10.Content = "Por favor preencha todos os campos";
+            }
+            else
+            {
+                if (!IsValid(textBox5.Text) || !IsValid(textBox6.Text))
+                {
+                    textBox5.Text = "";
+                    textBox6.Text = "";
+                    label10.Content = "Coordenadas GPS com formato inválido";
+                }
+                else
+                {
+                    latitude = Convert.ToDouble(textBox5.Text.Replace('.',','));
+                    longitude = Convert.ToDouble(textBox5.Text.Replace('.', ','));
+                    if (fac.demasiado_perto(latitude,longitude))
+                    {
+                        Popup1.IsOpen = true;
+                    }
+                }
+            }
+            if (inserir)
+            {
+                fac.registarRes(textBox3.Text, textBox4.Text, latitude, longitude);
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+                listarRestaurantes();
+                label10.Content = "Restaurante adicionado com sucesso";
+            }else
+            {
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+                label10.Content = "Restaurante não adicionado";
+            }
+            inserir = true;
+        }
+
+        private static bool IsValid(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9' || c==',' || c=='.')
+                    return false;
+            }
+
+            return true;
         }
 
         private void add_to_map_Click(object sender, RoutedEventArgs e)
         {
+            if (listView.SelectedItem != null)
+            {
                 Estabelecimento est = (Estabelecimento)listView.SelectedItems[0];
                 //Console.WriteLine(est.nome + "         " + est.latitude + "             " + est.longitude);
                 Pushpin pushpin = new Pushpin();
                 pushpin.Tag = est.id_est;
                 MapLayer.SetPosition(pushpin, new Location(est.latitude, est.longitude));
+                pushpin.Location = new Location(est.latitude, est.longitude);
                 myMap.Children.Add(pushpin);
-            //draw_route();
+                label7.Content = "Estabelecimento adicionado";
+            }else
+            {
+                label7.Content = "Por favor selecione um estabelecimento";
+            }
+            draw_route();
         }
 
-        //nao funciona
+        
         private void draw_route()
         {
-            //List<Location> vertices = new List<Location>();
+            clear_route();
             LocationCollection vertices = new LocationCollection();
             var pushPins = this.myMap.Children.OfType<Pushpin>();
             for (int i = 0; i < pushPins.Count(); i++)
             {
                 Pushpin p = pushPins.ElementAt(i);
-                Console.WriteLine("tou aqui"+p.Location);
-                if (p.Location == null)
-                {
-                    Console.WriteLine("tou qui 2");
-                }
                 vertices.Add(p.Location);
             }
             MapPolyline polyline = new MapPolyline();
@@ -159,61 +227,84 @@ namespace BackOffice
             myMap.Children.Add(polyline);
         }
 
-
-        private void remove_from_map_Click(object sender, RoutedEventArgs e)
+        private void clear_route()
         {
-                Estabelecimento est = (Estabelecimento)listView.SelectedItems[0];
-                var pushPins = this.myMap.Children.OfType<Pushpin>();
-            
-                for(int i=pushPins.Count()-1; i >= 0; i--)
+            int intTotalChildren = myMap.Children.Count - 1;
+            for (int intCounter = intTotalChildren; intCounter > 0; intCounter--)
             {
-                Pushpin p = pushPins.ElementAt(i);
-                if (p.Tag.Equals(est.id_est))
+                if (myMap.Children[intCounter].GetType() == typeof(MapPolyline))
                 {
-                    myMap.Children.Remove(p);
+                    MapPolyline ucCurrentChild = (MapPolyline)myMap.Children[intCounter];
+                    myMap.Children.Remove(ucCurrentChild);
                 }
             }
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+
+        private void remove_from_map_Click(object sender, RoutedEventArgs e)
+        {
+            if (listView.SelectedItem != null)
+            {
+                Estabelecimento est = (Estabelecimento)listView.SelectedItems[0];
+                var pushPins = this.myMap.Children.OfType<Pushpin>();
+                for (int i = pushPins.Count() - 1; i >= 0; i--)
+                {
+                    Pushpin p = pushPins.ElementAt(i);
+                    if (p.Tag.Equals(est.id_est))
+                    {
+                        myMap.Children.Remove(p);
+                    }
+                }
+                label7.Content = "Estabelecimento removido";
+            }
+            else
+            {
+                label7.Content = "Por favor selecione um estabelecimento";
+            }
+            draw_route();
+        }
+
+        private void limpar_selecao_Click(object sender, RoutedEventArgs e)
         {
             listView.SelectedItem = null;
             myMap.Children.Clear();
+            label7.Content = "Todos os estabelecimentos removidos";
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void criar_plano_Click(object sender, RoutedEventArgs e)
         {
             List<int> estabelecimentos=new List<int>();
-            //Estabelecimento est = (Estabelecimento)listView.SelectedItems[0];
             var pushPins = this.myMap.Children.OfType<Pushpin>();
-
-            for (int i = 0; i < pushPins.Count(); i++)
+            if (pushPins.Count() > 0)
             {
-                Pushpin p = pushPins.ElementAt(i);
-                estabelecimentos.Add(Convert.ToInt32(p.Tag.ToString()));
+                for (int i = 0; i < pushPins.Count(); i++)
+                {
+                    Pushpin p = pushPins.ElementAt(i);
+                    estabelecimentos.Add(Convert.ToInt32(p.Tag.ToString()));
+                }
+                fac.criarPlano(id_fiscal, estabelecimentos);
+                label7.Content = "Plano criado com sucesso";
+            }else
+            {
+                label7.Content = "Não selecionou nenhum estabelecimento a inspecionar";
             }
-            fac.criarPlano(id_fiscal,estabelecimentos);
-           /* foreach(int i in estabelecimentos)
-            {
-                Console.WriteLine(i);
-            }*/
-            label7.Content = "Plano criado com sucesso";
-            //Console.WriteLine(t);
-
         }
 
-        private void button7_Click(object sender, RoutedEventArgs e)
+        private void converter_xml_Click(object sender, RoutedEventArgs e)
         {
-            
-            Voz est = (Voz)listView1.SelectedItems[0];
-            //fac.playAudio(est.id_voz);
-            string xml=fac.convertXML(est.id_voz);
-            //String xml = fac.pa("", 2);
-            textBox.Text = xml;
-            label6.Content="Ficheiro convertido com sucesso";
+            if (listView1.SelectedItem != null)
+            {
+                Voz est = (Voz)listView1.SelectedItems[0];
+                string xml = fac.convertXML(est.id_voz);
+                textBox.Text = xml;
+                label6.Content = "Ficheiro convertido com sucesso";
+            }else
+            {
+                label6.Content = "Por favor selecione um ficheiro a converter";
+            }
         }
 
-        private void button3_Click(object sender, RoutedEventArgs e)
+        private void criar_relatorio_Click(object sender, RoutedEventArgs e)
         {
             
             if (listView2.SelectedItem != null)
@@ -222,10 +313,13 @@ namespace BackOffice
                 int visita = Convert.ToInt32(vis.id_vis);
                 CriarRelatorio newWindow = new CriarRelatorio(visita);
                 newWindow.Show();
+            }else
+            {
+                label8.Content = "Por favor escolha uma inspeção do qual deseja criar relatório";
             }
         }
 
-        private void button4_Click(object sender, RoutedEventArgs e)
+        private void consultar_relatorio_Click(object sender, RoutedEventArgs e)
         {
             if (listView3.SelectedItem != null)
             {
@@ -233,7 +327,62 @@ namespace BackOffice
                 int visita = Convert.ToInt32(vis.id_vis);
                 ConsultarRelatorio newWindow = new ConsultarRelatorio(visita);
                 newWindow.Show();
+            }else
+            {
+                label9.Content = "Por favor seleciona um relatório a consultar";
             }
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            inserir = true;
+            Popup1.IsOpen = false;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            inserir = false;
+            Popup1.IsOpen = false;
+        }
+
+        private void enviar_relatorio_Click(object sender, RoutedEventArgs e)
+        {
+            if (listView3.SelectedItem != null)
+            {
+                if (!textBox2.Text.Equals(""))
+                {
+                    visitaDTO vis = (visitaDTO)listView2.SelectedItems[0];
+                    int visita = Convert.ToInt32(vis.id_vis);
+                    Relatorio r = new Relatorio(visita);
+                    r.enviarRelatorio(textBox2.Text, vis.desc);
+                    label9.Content = "Relatório enviado com sucesso";
+                }else
+                {
+                    label9.Content = "Por favor insira o e-mail do recepiente";
+                }
+            }else
+            {
+                label9.Content = "Por favor seleciona um relatório a enviar";
+            }
+        }
+
+
+        private void GetResponse(Uri uri, Action<Response> callback)
+        {
+            WebClient wc = new WebClient();
+            wc.OpenReadCompleted += (o, a) =>
+            {
+                if (callback != null)
+                {
+                    DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Response));
+                    callback(ser.ReadObject(a.Result) as Response);
+                }
+            };
+            wc.OpenReadAsync(uri);
+        }
+
+        
+
+        
     }
 }
