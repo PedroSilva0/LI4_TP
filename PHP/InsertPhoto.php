fe87b6198da6aebfea8cf082ae69fcd0495cdb97
DiogoAFD <diogo_afd@hotmail.com>
2016-06-19 01:48:10 +0100

    php
    php

--- PHP/InsertPhoto.php
+++ PHP/InsertPhoto.php
@@ -2,9 +2,10 @@
 	require_once(dirname(__FILE__).'/ConnectionInfo.php');
 
 	
-if (isset($_POST['Foto']) && isset($_POST['IdVis']))
+if (isset($_POST['Descricao']) && isset($_POST['Foto']) && isset($_POST['IdVis']))
 {
 	//Get the POST variables
+	$desc = $_POST['Descricao'];
 	$foto = $_POST['Foto'];
 	$idVis = $_POST['IdVis'];
 	
@@ -23,10 +24,10 @@ if (isset($_POST['Foto']) && isset($_POST['IdVis']))
 		$imgData = base64_decode($foto);
 		
 		//Insert foto
-		$query = "INSERT INTO Foto(foto_file,visita) VALUES
-					(CONVERT(VARBINARY(MAX), ?), ?)";
+		$query = "INSERT INTO Foto(descricao,foto_file,visita) VALUES
+					(?, CONVERT(VARBINARY(MAX), ?), ?)";
 		//$parameters = array($foto, $idVis);
-		$parameters = array($imgData, $idVis);
+		$parameters = array($desc, $imgData, $idVis);
 
 		//Execute query
 		$stmt = sqlsrv_query($connectionInfo->conn, $query, $parameters);
